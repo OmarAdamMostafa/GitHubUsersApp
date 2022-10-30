9447d154e64392bdd3a9e04636f4395718d25e13
OmarAdamMostafa <115920338+OmarAdamMostafa@users.noreply.github.com>
2022-10-30 15:48:33 +0200

    Commit #2.2
    -Added a part of the search bar and error functionality in context.js and Search.js.
    -Finished the requesting rate limit part.

--- src/context/context.js
+++ src/context/context.js
@@ -12,7 +12,30 @@ const GithubProvider = ({children}) => {
     const [githubUser,setGitHubUser] = useState(mockUser);
     const [repos,setRepos] = useState(mockRepos);
     const [followers,serFollowers] = useState(mockFollowers);
-    return <GithubContext.Provider value={{githubUser,repos,followers}}>{children}</GithubContext.Provider>
+    const [requests,setRequests] = useState(0);
+    const [loading,setLoading] = useState(false);
+    const [error,setError] = useState({show:false,msg:''})
+
+    const checkRequests = () => {
+        axios(`${rootUrl}/rate_limit`)
+        .then((response)=>{
+            const {data} = response; 
+            let {rate:{remaining}} = data;
+            setRequests(remaining);
+            if(remaining === 0){
+                toggleError(true,'Sorry, You have exceeded your hourly rate limit!')
+            }
+        })
+        .catch((err)=>console.log(err));
+    }
+
+    function toggleError(show = false, msg = ''){
+        setError({show,msg});
+    }
+
+    useEffect(checkRequests,[])
+
+    return <GithubContext.Provider value={{githubUser,repos,followers,requests,error}}>{children}</GithubContext.Provider>
 }
 
 export {GithubProvider,GithubContext}
